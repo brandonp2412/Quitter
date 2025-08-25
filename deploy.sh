b3d2d2bb79ddbce021224f7c65355b7c4c140e15
Brandon Dick <brandon@presley.nz>
2025-08-25 14:39:04 +1200

    Specify device for screenshots

--- deploy.sh
+++ deploy.sh
@@ -223,7 +223,7 @@ generate_screenshots() {
             export QUITTER_DEVICE_TYPE="$device_type"
             
             # Try to generate screenshots
-            if flutter drive --profile --driver=test_driver/integration_test.dart --target=integration_test/screenshot_test.dart; then
+            if flutter drive --profile --driver=test_driver/integration_test.dart --target=integration_test/screenshot_test.dart -d $avd_name; then
                 print_success "Screenshots generated successfully for '$avd_name'"
             else
                 print_warning "Screenshot generation failed for '$avd_name'"
