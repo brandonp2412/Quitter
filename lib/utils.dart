2d0af6606188d7020af818bc743a0b524d1839e3
Brandon Dick <brandon@presley.nz>
2025-08-25 14:04:37 +1200

    Start counting days at 1 instead of zero

--- lib/utils.dart
+++ lib/utils.dart
@@ -10,5 +10,5 @@ int daysCeil(String dateStr) {
 
   final quitDateOnly = DateTime(quitDate.year, quitDate.month, quitDate.day);
   final nowDateOnly = DateTime(now.year, now.month, now.day);
-  return nowDateOnly.difference(quitDateOnly).inDays;
+  return nowDateOnly.difference(quitDateOnly).inDays + 1;
 }
