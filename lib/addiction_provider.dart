95756b40849530a279ed42afb7ead08a3798dbd1
Brandon Dick <brandon@presley.nz>
2025-09-14 13:01:35 +1200

    Fix error on non-android platforms

--- lib/addiction_provider.dart
+++ lib/addiction_provider.dart
@@ -1,4 +1,5 @@
 import 'dart:convert';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:quitter/utils.dart';
 import 'package:shared_preferences/shared_preferences.dart';
@@ -62,7 +63,8 @@ class AddictionProvider extends ChangeNotifier {
       _pref?.setString(key, value);
     }
     loadAddictions();
-    widgetChannel.invokeMethod('updateWidget');
+    if (defaultTargetPlatform == TargetPlatform.android)
+      widgetChannel.invokeMethod('updateWidget');
   }
 
   String? get quitAlcohol => _alcohol;
