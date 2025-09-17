dc95b519c5d0d010376a4a7951939e24ffb9fded
Brandon Dick <brandon@presley.nz>
2025-09-17 16:47:21 +1200

    Auto-capitalize custom entries

--- lib/quit_page_template.dart
+++ lib/quit_page_template.dart
@@ -311,6 +311,7 @@ class _QuitPageTemplateState extends State<QuitPageTemplate> {
                           focusNode: _textNode,
                           onTap: () => selectAll(controller),
                           controller: controller,
+                          textCapitalization: TextCapitalization.sentences,
                           decoration: InputDecoration(
                             hintText: '1',
                             floatingLabelBehavior: FloatingLabelBehavior.always,
