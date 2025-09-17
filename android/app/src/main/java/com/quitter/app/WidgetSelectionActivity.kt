7cef1704c198a154e9f2b9b4714008978f015364
Brandon Dick <brandon@presley.nz>
2025-09-17 16:38:16 +1200

    Only show quit addictions when picking widget

--- android/app/src/main/java/com/quitter/app/WidgetSelectionActivity.kt
+++ android/app/src/main/java/com/quitter/app/WidgetSelectionActivity.kt
@@ -4,6 +4,7 @@ import android.app.Activity
 import android.appwidget.AppWidgetManager
 import android.content.Intent
 import android.os.Bundle
+import androidx.core.content.edit
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
@@ -13,11 +14,8 @@ class WidgetSelectionActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_widget_selection)
-
-        // Set the result to CANCELED initially
         setResult(RESULT_CANCELED)
 
-        // Get the widget ID from the intent
         appWidgetId = intent?.extras?.getInt(
             AppWidgetManager.EXTRA_APPWIDGET_ID,
             AppWidgetManager.INVALID_APPWIDGET_ID
@@ -40,27 +38,34 @@ class WidgetSelectionActivity : Activity() {
             AddictionItem("vaping", "Vaping", R.drawable.ic_air, 0xFF06B6D4.toInt()),
             AddictionItem("smoking", "Smoking", R.drawable.ic_eco, 0xFF10B981.toInt()),
             AddictionItem("marijuana", "Marijuana", R.drawable.ic_grass, 0xFF84E680.toInt()),
-            AddictionItem("nicotine_pouches", "Nicotine Pouches", R.drawable.ic_scatter_plot, 0xFFF59E0B.toInt()),
+            AddictionItem(
+                "nicotine_pouches",
+                "Nicotine Pouches",
+                R.drawable.ic_scatter_plot,
+                0xFFF59E0B.toInt()
+            ),
             AddictionItem("opioids", "Opioids", R.drawable.ic_medication, 0xFFEC4899.toInt()),
             AddictionItem("social_media", "Social Media", R.drawable.ic_public, 0xFF8B5CF6.toInt()),
             AddictionItem("pornography", "Pornography", R.drawable.ic_block, 0xFFF43F5E.toInt())
         )
 
-        recyclerView.adapter = AddictionAdapter(addictions) { addiction ->
+        val prefs = getSharedPreferences("FlutterSharedPreferences", MODE_PRIVATE)
+        val quitAddictions = addictions.filter { addiction -> 
+            prefs.getString("flutter.${addiction.key}", null) != null 
+        }
+
+        recyclerView.adapter = AddictionAdapter(quitAddictions) { addiction ->
             selectAddiction(addiction.key)
         }
     }
 
     private fun selectAddiction(addictionKey: String) {
-        // Save the selection
-        val prefs = getSharedPreferences("QuitTrackerWidget", MODE_PRIVATE)
-        prefs.edit().putString("selected_$appWidgetId", addictionKey).apply()
+        val prefs = getSharedPreferences("FlutterSharedPreferences", MODE_PRIVATE)
+        prefs.edit { putString("flutter.selected_$appWidgetId", addictionKey) }
 
-        // Update the widget
         val appWidgetManager = AppWidgetManager.getInstance(this)
         QuitTrackerWidget.updateAppWidget(this, appWidgetManager, appWidgetId)
 
-        // Return success
         val resultValue = Intent()
         resultValue.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, appWidgetId)
         setResult(RESULT_OK, resultValue)
