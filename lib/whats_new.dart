0db054d853f547c7dd364f51801acb709dd905d1
Brandon Dick <brandon@presley.nz>
2025-09-18 18:45:30 +1200

    Fix "What's new" page on web

--- lib/whats_new.dart
+++ lib/whats_new.dart
@@ -37,9 +37,7 @@ class _WhatsNewState extends State<WhatsNew> {
   }
 
   Future<List<Changelog>> getChangelogFiles(BuildContext context) async {
-    final manifestContent = await DefaultAssetBundle.of(
-      context,
-    ).loadString('AssetManifest.json');
+    final manifestContent = await rootBundle.loadString('AssetManifest.json');
     final manifestMap = json.decode(manifestContent) as Map<String, dynamic>;
 
     final files = manifestMap.keys
@@ -49,7 +47,6 @@ class _WhatsNewState extends State<WhatsNew> {
     files.sort((a, b) {
       final aName = a.split('/').last.split('.').first;
       final bName = b.split('/').last.split('.').first;
-
       final aNum = int.tryParse(aName) ?? 0;
       final bNum = int.tryParse(bName) ?? 0;
       return bNum.compareTo(aNum);
@@ -57,28 +54,24 @@ class _WhatsNewState extends State<WhatsNew> {
 
     final result = <Changelog>[];
     for (final path in files) {
-      try {
-        final content = await rootBundle.loadString(path);
-        final filename = path.split('/').last.replaceAll('.txt', '');
+      final content = await rootBundle.loadString(path);
+      final filename = path.split('/').last.replaceAll('.txt', '');
+      final timestamp = int.tryParse(filename);
 
-        final timestamp = int.tryParse(filename);
-        if (timestamp == null || filename.isEmpty) {
-          print('Skipping invalid changelog file: $path');
-          continue;
-        }
-
-        result.add(
-          Changelog(
-            name: filename,
-            created: DateFormat.yMMMd().format(
-              DateTime.fromMillisecondsSinceEpoch(timestamp * 1000),
-            ),
-            content: content,
-          ),
-        );
-      } catch (e) {
-        print('Error loading changelog file $path: $e');
+      if (timestamp == null || filename.isEmpty) {
+        print('Skipping invalid changelog file: $path');
+        continue;
       }
+
+      result.add(
+        Changelog(
+          name: filename,
+          created: DateFormat.yMMMd().format(
+            DateTime.fromMillisecondsSinceEpoch(timestamp * 1000),
+          ),
+          content: content,
+        ),
+      );
     }
 
     return result;
